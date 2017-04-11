fd224384e90096a90ddb400c2863d706f1d3bcbe
triployd <xiangxiao.wang.ce@gmail.com>
2017-04-11 17:03:10 +0800

    kNN finished

--- chapter_2/kNN.py
+++ chapter_2/kNN.py
@@ -6,6 +6,7 @@ Created on Mon Apr 10 16:20:06 2017
 """
 
 from numpy import *
+from os import listdir
 import operator
 
 def createDataSet():
@@ -15,7 +16,7 @@ def createDataSet():
 
 def classify0(inX, dataSet, labels, k):
     # return predicted label
-    dataSetSize = dataSet.shape[0]
+    dataSetSize = dataSet.shape[0] # m value
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis=1)
@@ -42,5 +43,73 @@ def file2matrix(filename):
         classLabelVector.append(int(listFromLine[-1]))
         index += 1
     return returnMat, classLabelVector
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0) # min vals for each COLUMN
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet / tile(ranges, (m,1))
+    return normDataSet, ranges, minVals
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:], normMat[numTestVecs : m, :], datingLabels[numTestVecs:m], 3)
+        print "classifier result: %d, the real answer: %d" % (classifierResult, datingLabels[i])
+        if (classifierResult != datingLabels[i]) : errorCount += 1.0
+    print "total error rate: %f" % (errorCount / float(numTestVecs))
+
+def classifyPerson():
+    resultList = ['shabi', 'yiban', 'good' ]
+    percentTats = float(raw_input("percentage of time spent playing video games?"))
+    ffMiles = float(raw_input("frequent flier miles earned each year?"))
+    iceCream = float(raw_input("litres of ice cream consumed each year?"))
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt');
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles, percentTats, iceCream])
+    classifierResult = classify0(inArr - minVals/ranges, normMat, datingLabels, 3)
+    print "you will probably think this person is: ", resultList[classifierResult - 1]
+
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32*i+j] = int(lineStr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('trainingDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print "classifier result: %d, the real answer: %d" % (classifierResult, classNumStr)
+        if(classifierResult != classNumStr): errorCount += 1.0
+    print "\nthe total number of errors: %d" % errorCount
+    print "\nthe total error rate: %f" % (errorCount/float(mTest))
 
 print("kNN loaded!");
